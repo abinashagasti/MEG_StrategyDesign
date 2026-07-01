41031931a9f25f910c0bc70bbcec9bde9476a543
abinashagasti <abinashagastiof99@gmail.com>
2026-07-01 16:36:47 +0530

    Add selectable pursuer policies

--- main.m
+++ main.m
@@ -10,8 +10,10 @@ pursuer_position = [0;-1]; % Pursuer position 2*1
 evader_positions = [0;1]; % Evader positions 2*n
 target_position = [1;-0.5]; % Target position
 timestep = 0.01;
+pursuer_policy = "closest_next_step";
+% Options: "closest_next_step", "standard", "squaresum", "squaresump", "heuristic", "closest"
 
-env = Environment(n,timestep,pursuer_position,evader_positions,target_position);
+env = Environment(n,timestep,pursuer_position,evader_positions,target_position,pursuer_policy);
 
 %%
 
